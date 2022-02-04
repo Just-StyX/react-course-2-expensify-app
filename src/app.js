c2da60424d1902d4e6532533314918c1c2040ccf
Justice Sam <fjaccobian@gmail.com>
2022-02-04 13:11:09 +0000

    Setup production webpack build

--- src/app.js
+++ src/app.js
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import AppRouter from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
-import { addExpense } from "./actions/expenses";
-import getVisibleExpenses from "./selectors/expenses";
 import { Provider } from "react-redux";
 import './styles/styles.scss'
 import 'normalize.css/normalize.css'
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,38 @@
 const path = require('path');
 
-module.exports = {
-    mode: 'development',
-    entry: './src/app.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'public'),
-        publicPath: 'public'
-    },
-    module: {
-        rules: [{
-            use: {loader: 'babel-loader'},
-            test: /\.js$/,
-            exclude: /node_modules/
-        },{
-            test: /\.s?css/,
-            use: ['style-loader', 'css-loader', 'sass-loader']
-        }]
-    },
-    devtool: 'source-map',
-    devServer: {
-        static: {
-          directory: path.resolve(__dirname, 'public')
+//const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = () => {
+    //const CSSExtract = new ExtractTextPlugin('styles.css')
+    return {
+        mode: 'production',
+        entry: './src/app.js',
+        output: {
+            filename: 'bundle.js',
+            path: path.resolve(__dirname, 'public'),
+            publicPath: 'public'
         },
-        historyApiFallback: true  
-    },
+        module: {
+            rules: [{
+                use: {loader: 'babel-loader'},
+                test: /\.js$/,
+                exclude: /node_modules/
+            },{
+                test: /\.s?css/,
+                use: [MiniCssExtractPlugin.loader, {loader: "css-loader", options: { sourceMap: true}}, {loader: 'sass-loader', options: {sourceMap: true}}]
+                
+            }]
+        },
+        plugins: [new MiniCssExtractPlugin({filename: "styles.css"})],
+        //plugins: [ CSSExtract ],
+        devtool: 'source-map',
+        devServer: {
+            static: {
+              directory: path.resolve(__dirname, 'public')
+            },
+            historyApiFallback: true  
+        },
+    }
 }
+ 
